52194adbfc28b13a1b33c9cd9a2d4b594e30e6f4
André Luiz <andre.luizfr2@gmail.com>
2024-02-04 11:31:07 -0300

    feat: building breadcrumb component

---
+++ components/Breadcrumb/index.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import { BreadcrumbContainer, BreadcrumbLink, BreadcrumbLinks, PageDescription, PageTitle } from "./styles";
+import React from "react";
+
+interface breadcrumbContainerProps extends React.HTMLAttributes<HTMLElement>{
+  ordenedLabels: string[];
+  pageDescription: string;
+}
+
+export default function Breadcrumb({ ordenedLabels, pageDescription }: breadcrumbContainerProps) {
+
+  function convertLabelToLink(label: string) {
+    switch(label){
+      case "Home": return "/";
+      case "Quem Somos": return "/about";
+      case "Agendar Consulta": return "/schedule";
+      default: return "/";
+    }
+  }
+  return (
+    <BreadcrumbContainer>
+      <BreadcrumbLinks>
+        {ordenedLabels.map((label, index)=>{
+          if(index===ordenedLabels.length-1) {
+            return (
+              <Link href={convertLabelToLink(label)} key={"BreadcumbLink"+index}>
+                <BreadcrumbLink>{label}</BreadcrumbLink>
+              </Link>
+            )
+          }
+          else {
+            return (
+              <>
+                <Link href={convertLabelToLink(label)} key={"BreadcumbLink"+index}>
+                  <BreadcrumbLink>{label}</BreadcrumbLink>
+                </Link>
+                {">"}
+              </>
+            );
+          }
+        })}
+      </BreadcrumbLinks>
+      
+      <PageTitle>
+        {ordenedLabels[ordenedLabels.length-1]}
+      </PageTitle>
+
+      <PageDescription>
+        {pageDescription}
+      </PageDescription>
+
+    </BreadcrumbContainer>
+  )
+}
---
+++ components/Breadcrumb/styles.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const BreadcrumbContainer = styled.aside<{}>`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+  padding: 2rem 6rem;
+
+  background-color: var(--background-secondary-color);
+`;
+
+export const BreadcrumbLinks = styled.div<{}>`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: .5rem;
+
+  color: var(--font-secondary-color); //para ">"
+  font-size: var(--font-size-smaller); //para ">"
+  font-weight: var(--font-weigth-light); //para ">"
+`;
+
+export const BreadcrumbLink = styled.span<{}>`
+  color: var(--font-secondary-color);
+  font-size: var(--font-size-small);
+  font-weight: var(--font-weigth-bold);
+  
+  cursor: pointer;
+`
+
+export const PageTitle = styled.p<{}>`
+  color: var(--font-secondary-color);
+  font-size: var(--font-size-xl);
+  font-weight: var(--font-weigth-bold);
+`;
+
+export const PageDescription = styled.p<{}>`
+  margin-bottom: 2rem;
+
+  color: var(--font-secondary-color);
+  font-size: var(--font-size-small);
+  font-weight: var(--font-weigth-light);
+`;
